dc07808c368fd8032e78a1a98db32a4d3d27c2e6
Jinliang <hou80houzhu@vip.qq.com>
2017-03-30 01:39:22 +0800

    pass

--- WEBINF/src/controller.js
+++ WEBINF/src/controller.js
@@ -21,9 +21,9 @@ Module({
     },
     action: function () {
         // if(this.checkPath(this.request.getURL())){
-            return this.getCetusView();
+        //     return this.getCetusView();
         // }else {
-        // return this.getDefaultPageView("index", {});
+        return this.getIndexView({});
         // }
     },
     checkPath: function (url) {
